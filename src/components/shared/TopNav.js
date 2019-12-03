fef2ba540df7c0a589cf3674bbfcfc4eeac8a9a3
oovg <office@vengist.com>
2019-12-03 10:25:06 -0800

    updates some initial graphics

--- src/components/shared/TopNav.js
+++ src/components/shared/TopNav.js
@@ -4,7 +4,7 @@ import { withRouter, Link } from 'react-router-dom';
 import { CurrentUserContext } from '../../contexts/Store';
 import BcToast from './BcToast';
 
-import Brand from '../../assets/PokéMol__Logo.svg';
+import Brand from '../../assets/moloch__logo.png';
 import './TopNav.scss';
 import useModal from './useModal';
 import Modal from './Modal';
